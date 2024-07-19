080ab2d31b664f9d44397a8dc46c1f38494d3186
Jefferson-Matheus <jeffersonmatheusu@gmail.com>
2024-07-19 20:16:23 -0300

    :bug: fix: Alteração da porta

--- src/server.ts
+++ src/server.ts
@@ -21,4 +21,4 @@ app.use((err:Error, req:Request, res:Response, next:NextFunction) => {
   return res.status(500).json({message: 'Internal Server Error'});
 });
 
-app.listen(3333, () => console.log('Server is running on port 3333'));
+app.listen(5000, () => console.log('Server is running on port 5000'));
