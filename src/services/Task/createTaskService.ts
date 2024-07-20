fb9bf7c8effe1b9c1c31494598986a4657afeca0
Jefferson-Matheus <jeffersonmatheusu@gmail.com>
2024-07-19 22:24:38 -0300

    :sparkles: feat: Previnindo tarefas com o mesmo titulo

--- src/services/Task/createTaskService.ts
+++ src/services/Task/createTaskService.ts
@@ -3,6 +3,16 @@ import { CreateTaskInterface } from "../../types/requestTypes";
 
 class CreateTaskService{
     async execute({title,description}:CreateTaskInterface){
+        const taskAlreadyExists = await prismaClient.task.findFirst({
+            where:{
+                title:title
+            }
+        })
+
+        if(taskAlreadyExists){
+            throw new Error('Task already exists');
+        }    
+      
         const task = await prismaClient.task.create({
             data:{
                 title,
